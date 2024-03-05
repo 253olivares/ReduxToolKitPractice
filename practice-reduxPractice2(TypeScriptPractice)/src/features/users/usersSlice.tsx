160823ddf2bc481c2ae2386d2ea12fbed93e5b83
Miguel Olivares <67510851+253olivares@users.noreply.github.com>
2024-03-05 13:49:27 -0500

    Added comments

--- practice-reduxPractice2(TypeScriptPractice)/src/features/users/usersSlice.tsx
+++ practice-reduxPractice2(TypeScriptPractice)/src/features/users/usersSlice.tsx
@@ -1,17 +1,21 @@
+// create slice
 import { createSlice } from "@reduxjs/toolkit";
 import { RootState } from "../../app/store";
 
+// will fucntion more in the same way as post but just less as all it does it house our users no additional changes
 type initialStateType = {
     id:string,
     name:string
 }
 
+// set initial users state
 const initialState:Array<initialStateType> = [
     {id:'0', name:"dude Lebowski"},
     {id:'1', name:'Neil Young'},
     {id:'2', name:'Dave Gray'}
 ]
 
+// create a slice which just sets our state
 const userSlice = createSlice({
     name:'users',
     initialState,
@@ -20,6 +24,8 @@ const userSlice = createSlice({
     }
 })
 
+// create a select all users functions which just fetches state for our components
 export const selectAllUsers = (state: RootState) => state.users;
 
+// export our reducer
 export default userSlice.reducer;
